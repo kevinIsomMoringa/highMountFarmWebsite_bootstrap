8a6d3478c082b0f4146688d0acbd9a533c35319a
kevinIsomMoringa <kevin.isom@student.moringaschool.com>
2022-09-16 16:21:23 +0300

    Google AppScript backend files

---
+++ backEnd_Code/APIgateway.js
@@ -0,0 +1,30 @@
+// let workSheetID =
+
+function json(sheetName) {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet()
+    const sheet = spreadsheet.getSheetByName(sheetName)
+    const data = sheet.getDataRange().getValues()
+    const jsonData = convertToJson(data)
+    return ContentService
+          .createTextOutput(JSON.stringify(jsonData))
+          .setMimeType(ContentService.MimeType.JSON)
+}
+
+function convertToJson(data) {
+    const headers = data[0]
+    const raw_data = data.slice(1,)
+    let json = []
+    raw_data.forEach(d => {
+        let object = {}
+        for (let i = 0; i < headers.length; i++) {
+            object[headers[i]] = d[i]
+        }
+        json.push(object)
+    });
+    return json
+}
+
+function doGet(e) {
+    const path = e.parameter.path
+    return json(path)
+  }
